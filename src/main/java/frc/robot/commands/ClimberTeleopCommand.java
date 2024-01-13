cd511aa7fc0e6039b6432124aa85fc3943e6c65b
Wildcards <wildcards7506@gmail.com>
2024-01-13 12:08:21 -0600

    added teleop Shannon

--- src/main/java/frc/robot/commands/ClimberTeleopCommand.java
+++ src/main/java/frc/robot/commands/ClimberTeleopCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
+import frc.robot.ControlConfigs.PlayerConfigs;
 
 public class ClimberTeleopCommand extends Command{
     
@@ -11,6 +12,12 @@ public class ClimberTeleopCommand extends Command{
     
     @Override
     public void execute (){
-        
+    if (PlayerConfigs.climberdown) {
+            Robot.climbers.setclimbers(8);
+        } else if (PlayerConfigs.climberup) {
+            Robot.climbers.setclimbers(-8);
+        } else {
+            Robot.climbers.setclimbers(0);
+        }    
     } 
 }

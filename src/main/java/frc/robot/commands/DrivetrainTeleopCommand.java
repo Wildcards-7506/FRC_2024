8a2a0e8fe5f9074b8ea7ce434ba6af6752eeb729
Anthony Dang <anthonyd108108@gmail.com>
2024-03-12 17:23:10 -0500

    flipping directions, commented out for fixing other issues

--- src/main/java/frc/robot/commands/DrivetrainTeleopCommand.java
+++ src/main/java/frc/robot/commands/DrivetrainTeleopCommand.java
@@ -33,9 +33,9 @@ public class DrivetrainTeleopCommand extends Command{
         // This change makes the intake as the front of the robot, therefore
         // we will need to power the robot so that the intake points to the
         // middle of the field.
-        xInputSpeed = -xInputSpeed;
-        yInputSpeed = -yInputSpeed;
-        inputRot = -inputRot;
+        // xInputSpeed = -xInputSpeed;
+        // yInputSpeed = -yInputSpeed;
+        // inputRot = -inputRot;
 
         //Snap or align if needed, set drive if joystick inputs available, otherwise X
         if(PlayerConfigs.snapUp){

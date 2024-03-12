a3c8546365050361e174906cb93a8f705b385628
Anthony Dang <anthonyd108108@gmail.com>
2024-03-12 14:26:24 -0500

    front of robot is now intake for all drivers unless changed individually

--- src/main/java/frc/robot/commands/DrivetrainTeleopCommand.java
+++ src/main/java/frc/robot/commands/DrivetrainTeleopCommand.java
@@ -30,6 +30,13 @@ public class DrivetrainTeleopCommand extends Command{
             PlayerConfigs.fineTurnSpeed * PlayerConfigs.turnMovement : 
             PlayerConfigs.turnSpeed * PlayerConfigs.turnMovement;
 
+        // This change makes the intake as the front of the robot, therefore
+        // we will need to power the robot so that the intake points to the
+        // middle of the field.
+        xInputSpeed = -xInputSpeed;
+        yInputSpeed = -yInputSpeed;
+        inputRot = -inputRot;
+
         //Snap or align if needed, set drive if joystick inputs available, otherwise X
         if(PlayerConfigs.snapUp){
             double angle = Robot.teamColor.get() == Alliance.Red ? 180 : 0;
